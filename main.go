c55b8e322669fbd3476ecdebc7e5adf27498752c
Rob Lowcock <rob.lowcock@sainsburys.co.uk>
2018-06-15 12:21:23 +0100

    Solution

--- main.go
+++ main.go
@@ -19,5 +19,35 @@ func main() {
 }
 
 func CalculateHouses(input string) int {
-	return 0
+	pos := [2]int{0,0}
+	houses := [][2]int{pos}
+
+	for _, v := range input {
+		switch val := string(v); val {
+		case "^":
+			pos[1]++
+		case ">":
+			pos[0]++
+		case "<":
+			pos[0]--
+		case "v":
+			pos[1]--
+		}
+
+		if !contains(houses, pos) {
+			houses = append(houses, pos)
+		}
+	}
+
+	return len(houses)
+}
+
+func contains(haystack [][2]int, needle [2]int) bool {
+	for _, v := range haystack {
+		if v == needle {
+			return true
+		}
+	}
+
+	return false
 }
